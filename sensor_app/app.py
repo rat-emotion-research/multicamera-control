b7616c86dd70248d71ed078f2aa077102dffaff2
Andre Telfer <andretelfer@cmail.carleton.ca>
2021-07-16 18:36:54 -0400

    cameras serve individual views

--- sensor_app/app.py
+++ sensor_app/app.py
@@ -3,7 +3,7 @@ import subprocess
 import re
 from io import BytesIO
 from PIL import Image
-from flask import Flask, Response
+from flask import Flask, Response, render_template
 
 app = Flask(__name__)
 
@@ -74,6 +74,10 @@ def gen():
         small_frame = small_frame.tobytes("jpg", "rgb")
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + small_frame + b'\r\n\r\n')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/video_feed')
 def video_feed():
