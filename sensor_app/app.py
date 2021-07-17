c2dc3502cd9f9a6efcec348df5430cbfeaa0df07
Andre Telfer <andretelfer@cmail.carleton.ca>
2021-07-17 13:02:46 -0400

    Working camera stream

--- sensor_app/app.py
+++ sensor_app/app.py
@@ -1,6 +1,10 @@
 import ffmpeg
 import subprocess
 import re
+import numpy as np
+import cv2
+
+from multiprocessing import Queue, Process
 from io import BytesIO
 from PIL import Image
 from flask import Flask, Response, render_template
@@ -8,8 +12,11 @@ from flask import Flask, Response, render_template
 app = Flask(__name__)
 
 FPS = 30
-FORMAT = 'mjpeg'
+FORMAT = 'h264'
 BITRATE = '25M'
+CHUNK_SIZE = 2**10
+
+capture_queue = Queue()
 
 class VideoReader:
     def __init__(self):
@@ -18,10 +25,12 @@ class VideoReader:
 
     def get_frame_size(self):
         """Get the number of bytes in each image"""
-        vidcap_settings = subprocess.check_output('v4l2-ctl -V'.split())
-        vidcap_settings = vidcap_settings.decode()
-        match = re.search(r'Size Image\s*\:\s*(\d+)', vidcap_settings)
-        image_size = int(match.groups()[0])
+        # vidcap_settings = subprocess.check_output('v4l2-ctl -V'.split())
+        # vidcap_settings = vidcap_settings.decode()
+        # match = re.search(r'Size Image\s*\:\s*(\d+)', vidcap_settings)
+        # image_size = int(match.groups()[0])
+        # print(image_size)
+        image_size = CHUNK_SIZE
         return image_size
 
     def create_reader(self):
@@ -38,6 +47,7 @@ class VideoReader:
                 bufsize='128M',
                 format=FORMAT,
                 codec='copy',
+                chunk_size=CHUNK_SIZE,
                 **{'b:v': BITRATE}
             )
             .run_async(pipe_stdout=True))
@@ -63,17 +73,62 @@ def get_writer():
         .overwrite_output()
         .run_async(pipe_stdin=True))
 
-reader = VideoReader()
+def get_decoder():
+    """Created an FFMpeg Video Writer"""
+    return (ffmpeg
+        .input(
+            'pipe:', 
+            framerate=FPS,
+            format=FORMAT,
+            # pix_fmt='yuv420p',
+        )
+        .filter('fps', fps=12, round='up')
+        .filter('scale', 640, 480) # width, height
+        .output(
+            'pipe:',
+            bufsize='128M',
+            format='rawvideo',
+            pix_fmt='rgb24',
+            **{'b:v': '1M'}
+        )
+        .overwrite_output()
+        .run_async(pipe_stdin=True, pipe_stdout=True))
+
+decoder = get_decoder()
+
+def consume_frames(decoder, **kwargs):
+    reader = VideoReader()
+    count = 0
+    while True:
+        count += 1
+        # Empty the queue
+        while capture_queue.qsize() > 1:
+            try:
+                capture_queue.get(block=False)
+            except:
+                pass
+
+        raw_frame = reader.get_frame()
+        decoder.stdin.write(raw_frame)
+
+def make_jpg(capture_queue, decoder, **kwargs):
+    shape = (480, 640, 3) # Height, Width, Channels
+
+    while True:
+        frame = decoder.stdout.read(np.product(shape))
+        data = np.frombuffer(frame, np.uint8)
+        # print('data', data.shape)
+        image = Image.fromarray(data.reshape(*shape))
+        out = BytesIO()
+        image.save(out, 'JPEG')
+        out.seek(0)
+        capture_queue.put(out.read())
 
 def gen():
     while True:
-        raw_frame = reader.get_frame()
-        small_frame = raw_frame #.to_bytes()
-        # decoded_frame = Image.open(BytesIO(raw_frame))
-        # small_frame = decoded_frame.resize((320, 240))
-        # small_frame = small_frame.tobytes("jpg", "rgb")
+        out = capture_queue.get()
         yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + small_frame + b'\r\n\r\n')
+            b'Content-Type: image/jpeg\r\n\r\n' + out + b'\r\n')
 
 @app.route('/')
 def index():
@@ -83,3 +138,16 @@ def index():
 def video_feed():
     return Response(gen(),
         mimetype='multipart/x-mixed-replace; boundary=frame')
+
+shared_kwargs = {
+    'capture_queue': capture_queue,
+    'decoder': decoder
+}
+
+capture_process = Process(
+    name="capture", target=consume_frames, kwargs=shared_kwargs)
+capture_process.start()
+
+decoder_process = Process(
+    name="decoder", target=make_jpg, kwargs=shared_kwargs)
+decoder_process.start()
